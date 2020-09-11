65194e76c4051fd1a7779d4a21f85f5cc8593c79
Masoud <msoolymani@gmail.com>
2020-09-11 12:02:25 +0100

    ptactice

---
+++ sub/app.js
@@ -0,0 +1,10 @@
+myID = document.getElementById("myID");
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 0) {
+        myID.className = "slide show"
+    }
+};
+
+window.addEventListener("load", myScrollFunc);
