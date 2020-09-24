3833162a1deeeaeadcbdbbec6e1f6039f943b64b
Masoud <msoolymani@gmail.com>
2020-09-24 19:38:33 +0100

    bug fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ myID = document.getElementById("myID");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 10) {
+    if (y >= 1) {
         myID.className = "slide show"
     }
 };
